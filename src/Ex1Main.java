96cd5228d2ce79b5629f3105b14aa6145b88afb1
David <kaminerdavid5@gmail.com>
2024-12-09 21:55:04 +0200

    init commit

--- src/Ex1Main.java
+++ src/Ex1Main.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 import assignments.ex1.Ex1;
 
+import static assignments.ex1.Ex1.number2Int;
+
 /**
  * Intro2CS, Ex1 - very basic "main template"
  * Make sure your implementation of this main performs as the Ex1Sol.jar solution implement all needed functions.
@@ -10,34 +12,39 @@ public class Ex1Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String num1 = "", num2="", quit = "quit";
+        String output_base;
         while (!num1.equals(quit) && !num2.equals(quit)) {
             System.out.println();
             System.out.println("Ex1 class solution:");
             System.out.println("Enter a string as number#1 (or \"quit\" to end the program): ");
             num1 = sc.next();
-            if (!num1.equals("quit"))
-            {
-                // add your code here
-                if(Ex1.isNumber(num1)) {
-                    System.out.println("Num1 is valid ");
-                    System.out.println("Enter a string as number#2 (or \"quit\" to end the program): ");
-                    num2 = sc.next();
-                    if (num2.equals("quit"))
-                    {
-                        break;
-                    }
-                    else if (Ex1.isNumber(num2)){
-                        System.out.println("Num2 is valid ");
-                    }
-                    else {
-                        System.out.println("Num2 is invalid ");
-                    }
+
+            // add your code here
+            if(Ex1.isNumber(num1)) {
+                System.out.println("Num1 is valid ");
+                System.out.println("Num1 is worth " + number2Int(num1));
+
+                System.out.println("Enter a string as number#2 (or \"quit\" to end the program): ");
+                num2 = sc.next();
+                if (Ex1.isNumber(num2)){
+                    System.out.println("Num2 is valid ");
+                    System.out.println("Num2 is worth " + number2Int(num2));
+                    System.out.println("Enter a base for output: (a number [2,16]");
+                    output_base = sc.next();
+                    System.out.println(num1 + " + " + num2 + " = " + Ex1.int2Number(number2Int(num1) + number2Int(num2), Integer.parseInt(output_base)));
+                    System.out.println(num1 + " * " + num2 + " = " + Ex1.int2Number(number2Int(num1) * number2Int(num2), Integer.parseInt(output_base)));
+
+
                 }
                 else {
-                    System.out.println("Num1 is invalid ");
+                    System.out.println("Num2 is invalid ");
                 }
-                /////////////////////
             }
+            else {
+                System.out.println("Num1 is invalid ");
+            }
+                /////////////////////
+
 
 
 
--- src/assignments/ex1/Ex1.java
+++ src/assignments/ex1/Ex1.java
@@ -13,73 +13,106 @@ package assignments.ex1;
  * You should implement the following static functions:
  */
 public class Ex1 {
-        /**
-         * Convert the given number (num) to a decimal representation (as int).
-         * It the given number is not in a valid format returns -1.
-         * @param num a String representing a number in basis [2,16]
-         * @return
-         */
-        public static int number2Int(String num) {
-            int ans = -1;
-            // add your code here
-
-            ////////////////////
-            return ans;
+    /**
+     * Convert the given number (num) to a decimal representation (as int).
+     * It the given number is not in a valid format returns -1.
+     *
+     * @param num a String representing a number in basis [2,16]
+     * @return
+     */
+    public static int number2Int(String num) {
+        int ans = 0;
+        // add your code here
+        String[] parts = num.split("b");
+        String numberPart = parts[0];
+        String base = parts[1];
+        String valid = "0123456789ABCDEF";
+        int true_base = valid.indexOf(base.toCharArray()[0]);
+        int len = numberPart.length();
+        for(int i = 0; i<len; i++)
+        {
+            int k = valid.indexOf(numberPart.toCharArray()[i]);
+            ans += (int) (k*Math.pow(true_base, (len - i - 1)));
         }
-        /**
-         * This static function checks if the given String (g) is in a valid "number" format.
-         * @param a a String representing a number
-         * @return true iff the given String is in a number format
-         */
-        public static boolean isNumber(String a) {
-            boolean ans = true;
-            // add your code here.
-            String valid = "23456789ABCDEFG";
-            int index = a.indexOf('b');
-            String base, num;
+
+        ////////////////////
+        return ans;
+    }
 
 
-            if (index != -1 && index == a.lastIndexOf('b') && a.lastIndexOf('b') == a.length()-2
-                    && index != 0) {
-                base = a.split("b")[1];
+    /**
+     * This static function checks if the given String (g) is in a valid "number" format.
+     *
+     * @param a a String representing a number
+     * @return true iff the given String is in a number format
+     */
+    public static boolean isNumber(String a) {
+        boolean ans = true;
+        // add your code here.
+        String valid = "23456789ABCDEFG";
+        int index = a.indexOf('b');
+        String base, num;
 
-                if (!valid.contains(base))
-                {
-                    return false;
-                }
-                valid = "0123456789ABCDEFG";
-                num = a.split("b")[0];
-                valid = valid.split(base)[0];
-                for(int i=0; i < num.length(); i++){
-                    char b = num.charAt(i);
-                    if (valid.indexOf(b) == -1) {
-                        return false;
-                    }
-                }
-            }
-            else {
+
+        if (index != -1 && index == a.lastIndexOf('b') && a.lastIndexOf('b') == a.length() - 2
+                && index != 0) {
+            base = a.split("b")[1];
+
+            if (!valid.contains(base)) {
                 return false;
             }
-
-            ////////////////////
-            return ans;
+            valid = "0123456789ABCDEFG";
+            num = a.split("b")[0];
+            valid = valid.split(base)[0];
+            for (int i = 0; i < num.length(); i++) {
+                char b = num.charAt(i);
+                if (valid.indexOf(b) == -1) {
+                    return false;
+                }
+            }
+        } else {
+            return false;
         }
 
-        /**
-         * Calculate the number representation (in basis base)
-         * of the given natural number (represented as an integer).
-         * If num<0 or base is not in [2,16] the function should return "" (the empty String).
-         * @param num the natural number (include 0).
-         * @param base the basis [2,16]
-         * @return a String representing a number (in base) equals to num, or an empty String (in case of wrong input).
-         */
-        public static String int2Number(int num, int base) {
-            String ans = "";
-            // add your code here
+        ////////////////////
+        return ans;
+    }
 
-            ////////////////////
-            return ans;
+    /**
+     * Calculate the number representation (in basis base)
+     * of the given natural number (represented as an integer).
+     * If num<0 or base is not in [2,16] the function should return "" (the empty String).
+     *
+     * @param num  the natural number (include 0).
+     * @param base the basis [2,16]
+     * @return a String representing a number (in base) equals to num, or an empty String (in case of wrong input).
+     */
+    public static String int2Number(int num, int base) {
+        //String ans = "";
+        // add your code here
+        String valid = "0123456789ABCDEFG";
+
+        if (num < 0 || base < 2 || base > 16) {
+            return ""; // Return an empty string for invalid input
         }
+        StringBuilder ans = new StringBuilder();
+        while (num > 0) {
+            int remainder = num % base;// Find the remainder when num is divided by base
+            // Determine the character for the current digit:
+            // - '0' to '9' for values 0–9
+            // - 'A' to 'F' for values 10–1
+            char digit = (char) (remainder < 10 ? '0' + remainder : 'A' + (remainder - 10));
+            ans.append(digit);
+            num /= base;
+        }
+        // Reverse the result since the digits are generated in reverse order
+        return ans.reverse().toString() + 'b' + valid.charAt(base);
+        ////////////////////
+        //return ans;
+    }
+
+
+
 
         /**
          * Checks if the two numbers have the same value.
@@ -87,27 +120,35 @@ public class Ex1 {
          * @param n2 second number
          * @return true iff the two numbers have the same values.
          */
-        public static boolean equals(String n1, String n2) {
-            boolean ans = true;
-            // add your code here
-
-            ////////////////////
-            return ans;
+        public static boolean equals(String n1, String n2)
+        {
+            return number2Int(n1) == number2Int(n2);
         }
 
-        /**
-         * This static function search for the array index with the largest number (in value).
-         * In case there are more than one maximum - returns the first index.
-         * Note: you can assume that the array is not null and is not empty, yet it may contain null or none-valid numbers (with value -1).
-         * @param arr an array of numbers
-         * @return the index in the array in with the largest number (in value).
-         *
-         */
-        public static int maxIndex(String[] arr) {
-            int ans = 0;
-            // add your code here
 
-            ////////////////////
-            return ans;
+    /**
+     * This static function search for the array index with the largest number (in value).
+     * In case there are more than one maximum - returns the first index.
+     * Note: you can assume that the array is not null and is not empty, yet it may contain null or none-valid numbers (with value -1).
+     *
+     * @param arr an array of numbers
+     * @return the index in the array in with the largest number (in value).
+     */
+    public static int maxIndex(String[] arr) {
+        int ans = 0;
+        int value2, maxValue;
+        // add your code here
+        maxValue = number2Int(arr[0]);
+        for(int i = 0; i < arr.length - 1; i++) {
+            value2 = number2Int(arr[i+1]);
+            if(maxValue < value2)
+            {
+                ans = i+1;
+                maxValue = value2;
+            }
         }
+        ////////////////////
+        return ans;// Return the index of the largest number
+    }
+
 }
